5c18b678c6abd38981de7aa2a8ad1cc0c9ad3265
Emirhan Demir <32203408+emirhandemir922@users.noreply.github.com>
2020-12-24 20:53:56 +0300

    Add files via upload

--- Mail.py
+++ Mail.py
@@ -5,21 +5,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from zipfile import ZipFile
 import pandas as pd
+import clipboard
 import pdfcrowd
+import shutil
 import os
 import sys
 import time
+import smtplib
+import mimetypes
+from email.message import EmailMessage
 
-client = pdfcrowd.HtmlToPdfClient('demo', 'ce544b6ea52a5621fb9d55f8b542d14d')
 PATH_DRIVER = "chromedriver.exe"
 PATH_EXCEL = "Siparişleriniz.xlsx"
-FILES_PATH = "%USERPROFILE%/Downloads/*.zip"
-driver = webdriver.Chrome(PATH_DRIVER)
 
+current_working_directory = os.getcwd()
+client = pdfcrowd.HtmlToPdfClient('TheDifferent', 'ce544b6ea52a5621fb9d55f8b542d14d')
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory': current_working_directory}
+chrome_options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(PATH_DRIVER, options=chrome_options)
 
 data = pd.DataFrame(pd.read_excel(PATH_EXCEL),columns=['Alıcı', 'E-Posta'])
-data_Alıcı = data['Alıcı'].tolist()
+data_Alıcı = (data['Alıcı'] + " " + data['Alıcı']).tolist()
+data_Alıcı_pdf = (data['Alıcı']).tolist()
+print(data_Alıcı_pdf)
+for i in range(len(data_Alıcı)):
+    data_Alıcı[i] = data_Alıcı[i].split()
 data_Mail = data['E-Posta'].tolist()
 
 driver.get("https://earsivportal.efatura.gov.tr/intragiris.html")
@@ -52,33 +65,104 @@ try:
                                               "div[2]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/div/ul/li[2]/ul/li[2]/a").click()
 
         try:
-            date_start = WebDriverWait(driver, 20).until(
-                EC.presence_of_all_elements_located((By.ID, "date-gen__1024"))
+            calendar_start = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located((By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]"
+                                                               "/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div/img"))
             )
-            date_start[0].send_keys(input("Başlangıç Tarihi Giriniz(Örn: 08/09/2020) : "))
+            clipboard.copy(input("Başlangıç Tarihi Giriniz(Örn: 08/09/2020) : "))
+            calendar_start[0].click()
+            date_start = driver.find_element_by_id("date-gen__1024")
+            date_start.send_keys(clipboard.paste())
 
+            calender_end = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/"
+                                                    "div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div[1]/div/div/div/div[2]/div[2]/div/img")
+            clipboard.copy(input("Bitiş Tarihi Giriniz (Örn: 10/09/2020) : "))
+            calender_end.click()
             date_end = driver.find_element_by_id("date-gen__1025")
-            date_end.send_keys(input("Bitiş Tarihi Giriniz (Örn: 10/09/2020) : "))
+            date_end.send_keys(clipboard.paste())
 
             submit_button = driver.find_element_by_id("gen__1026").click()
-
             time.sleep(5)
 
-            for bill_index in range(len(data_Alıcı)):
+            bill_number = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]"
+                                                       "/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div[2]/div/div/table/tbody/tr[11]/td/span").text
+            bill_number = bill_number.split()
+
+            page_number = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]"
+                                                       "/div/div/div[2]/div[2]/div/div/div[2]/div/div/table/tbody/tr[11]/td/div/span[5]").text
+            page_number = page_number.replace("/", "")
+
+            bill_index = 0
+
+            for page_index in range(0, int(page_number)):
                 checkbox = driver.find_elements_by_class_name("csc-table-select")
 
                 for checkbox_index in range(1, len(checkbox)):
-                    checkbox[checkbox_index].click()
+                    approvebox = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]"
+                                                              "/div/div/div[2]/div[2]/div/div/div[2]/div/div/table/tbody/tr[" + str(checkbox_index) + "]/td[7]/a/i").get_attribute("class")
+                    person_name = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/div[2]/"
+                                                               "div[2]/div/div/div[2]/div/div/table/tbody/tr[" + str(checkbox_index) + "]/td[4]/span").text
+                    person_name = person_name.split()
 
-                    download = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/"
+                    if (approvebox == "fa fa-check" and person_name == data_Alıcı[bill_index]):
+                        checkbox[checkbox_index].click()
+
+                        download = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/"
                                                             "div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div[2]/div/div/div/div[5]/div/div/input").click()
+                        checkbox[checkbox_index].click()
 
-                    client.convertFileToFile('C:/Users/Emirhan/AppData/Local/Temp/Temp1_test.zip/3716810c-ad85-496e-b0f6-407922021ca0_f.html', data_Alıcı[bill_index] + '.pdf')
+                        time.sleep(5)
 
+                        for zip_item in os.listdir(current_working_directory):
+                            if zip_item.endswith(".zip"):
+                                with ZipFile(zip_item, 'r') as zipObj:
+                                    zipObj.extractall()
+                                    zipObj.close()
+                                    os.remove(zip_item)
 
+                        for html_file in os.listdir(current_working_directory):
+                            if html_file.endswith(".html"):
+                                client.convertFileToFile(html_file, data_Alıcı_pdf[bill_index] + '.pdf')
+                                os.remove(html_file)
 
-                next_page = driver.find_elements_by_class_name("csc-table-paging-btn")
-                next_page[2].click()
+                        for xml_file in os.listdir(current_working_directory):
+                            if xml_file.endswith(".xml"):
+                                os.remove(xml_file)
+
+                        recipient = data_Mail[bill_index]
+                        message = EmailMessage()
+                        sender = "emirhandemir922@gmail.com"
+                        password = 'E121011e'
+
+                        message['From'] = sender
+                        message['To'] = recipient
+                        message['Subject'] = 'Learning to send email from medium.com'
+                        body = 'Hello I am learning to send emails using Python!!!'
+
+                        message.set_content(body)
+                        mime_type, _ = mimetypes.guess_type(data_Alıcı_pdf[bill_index] + '.pdf')
+                        mime_type, mime_subtype = mime_type.split('/')
+
+                        with open(data_Alıcı_pdf[bill_index] + '.pdf', 'rb') as file:
+                            message.add_attachment(file.read(),
+                            maintype=mime_type,
+                            subtype=mime_subtype,
+                            filename=data_Alıcı_pdf[bill_index] + '.pdf')
+
+                        mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+                        mail_server.login(sender, password)
+                        mail_server.send_message(message)
+
+                        bill_index = bill_index + 1
+
+                        time.sleep(1)
+
+                next_page = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[2]/div/"
+                                                     "div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div[2]/div/div/table/tbody/tr[11]/td/div/span[6]")
+                next_page.click()
+                time.sleep(1)
+
+            mail_server.quit()
 
         finally:
             driver.refresh()
@@ -86,3 +170,5 @@ try:
         driver.refresh()
 finally:
     driver.refresh()
+
+print("İşleminiz bitmiştir.")
